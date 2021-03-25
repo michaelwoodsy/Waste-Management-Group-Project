da8063736518b6bd4d13ab4f40999a69bf6e0eb0
Niels van Antwerpen <nva40@uclive.ac.nz>
2021-03-24 20:17:40 -0700

    S302T200-111 #time 2m added javadoc and comments to the LoginRequired page ?document

--- frontend/src/components/LoginRequired.vue
+++ frontend/src/components/LoginRequired.vue
@@ -5,17 +5,19 @@
                 <h4>Login Required</h4>
             </div>
         </div>
-
+      <!-- User is not logged in-->
         <div class="row">
             <div class="col-12 col-sm-8 col-lg-6 col-xl-4 offset-sm-2 offset-lg-3 offset-xl-4 text-center mb-2">
                 <p class="text-muted">You must be logged in to {{page}}.</p>
                 <div class="form-row">
                     <div class="form-group col-6">
+                      <!-- Register Button -->
                         <router-link to="/register" class="no-underline">
                             <button type="button" class="btn btn-block btn-light">Register</button>
                         </router-link>
                     </div>
                     <div class="form-group col-6">
+                      <!-- Login Button -->
                         <router-link to="/login" class="no-underline">
                             <button class="btn btn-block btn-primary">Login</button>
                         </router-link>
