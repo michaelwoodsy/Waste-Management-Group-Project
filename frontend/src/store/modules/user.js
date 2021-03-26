e31223114640ed110d55d6f2344d896d1b8ce497
Tom Rizzi <tcr59@uclive.ac.nz>
2021-03-26 21:27:26 +1300

    S302T200-24 #time 1h 25m Implemented logging out and made so the user will remain logged in for the session. ?implement ?document
    - Renamed stores module.
    - Added docstrings to user store module.
    - Created cookieJar module.
    - Updated login store method to set userId cookie.
    - Created checkLoggedIn method in user store.
    - Added checkLoggedIn method to main.js so it runs on startup.

---
+++ frontend/src/store/modules/user.js
@@ -0,0 +1,82 @@
+import { User } from '../../Api'
+import { getCookie, setCookie, deleteCookie} from "../../../utils/cookieJar";
+
+export default {
+    debug: true,
+
+    /** Store state **/
+    state: {
+        message: 'Hello!',
+        loggedIn: false,
+        userId: null
+    },
+
+    /**
+     * Sets the user state as logged in, including the userId cookie.
+     * @param userId UserId of the user that is logged in
+     */
+    setLoggedIn(userId) {
+        this.state.loggedIn = true;
+        this.state.userId = userId;
+        setCookie('userId', this.state.userId, null);
+    },
+
+    /**
+     * Sets the current user as logged out.
+     */
+    setLoggedOut() {
+        this.state.loggedIn = false;
+        this.state.userId = null;
+        deleteCookie('userId');
+    },
+
+    /**
+     * Logs the user in, and sets the corresponding loggedIn value if successful
+     * @param username Username to send to api
+     * @param password Password to send to api
+     * @returns {Promise<unknown>} Axios response
+     */
+    login (username, password) {
+        // Return a promise for the api call
+        return new Promise((resolve, reject) => {
+            User.login(username, password)
+                .then((res) => {
+                    // Set logged in then resolve the promise
+                    this.setLoggedIn(res.data.userId);
+                    resolve(res)
+                })
+                .catch((err) => {
+                    // Set logged out then reject the promise
+                    this.setLoggedOut();
+                    reject(err)
+                })
+        })
+    },
+
+    /**
+     * Logs out the user, and updates the according loggedIn value
+     */
+    logout () {
+       this.setLoggedOut()
+    },
+
+    /**
+     * Checks if the current user is logged in, sets the app state accordingly
+     */
+    checkLoggedIn() {
+        // Ger userId from cookies
+        const userId = getCookie('userId');
+
+        // Check if the userId was null
+        if (userId === null) {
+            return
+        } else {
+            this.setLoggedIn(userId)
+        }
+
+        // Check if the logged in users session token is valid
+        User.getUserData(userId)
+            .then(() => this.setLoggedIn(userId))
+            .catch(() => this.setLoggedOut())
+    }
+}
