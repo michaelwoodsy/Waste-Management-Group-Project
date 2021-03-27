4e88d80229842509869cccd6fcc3d9ab8165db4d
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-03-28 10:40:42 +1300

    S302T200-25 #time 25m Added member since on profile page,only used to be on the private profile page but that has since been changed to a home page ?implement
    - Basically copied the code over from the old profile page

--- frontend/src/components/ProfilePage.vue
+++ frontend/src/components/ProfilePage.vue
@@ -85,6 +85,16 @@
         </div>
       </div>
 
+      <!-- Member Since -->
+      <div class="row">
+        <div class="col-6 text-right font-weight-bold">
+          <p>Member Since: </p>
+        </div>
+        <div class="col-6 ">
+          <p>{{dateJoined}} ({{dateSinceJoin}}) </p>
+        </div>
+      </div>
+
     </div>
 
   </div>
@@ -138,7 +148,63 @@ export default {
       this.email = response.data.email
 
       //Need to remove the street and number part of this address, just splice from the first ','
-      //this.homeAddress = response.data.homeAddress.slice(response.data.homeAddress.indexOf(",")+2);
+      this.homeAddress = response.data.homeAddress.slice(response.data.homeAddress.indexOf(",")+2);
+
+      this.dateJoined = response.data.created
+      this.timeCalculator(Date.parse(this.dateJoined))
+      this.dateJoined = this.dateJoined.substring(0, 10)
+    },
+
+    /**
+     * Calculates the time since the user joined in ms from the start of time (1970)
+     * @param joined date when the user joined
+     */
+    timeCalculator(joined) {
+      let dateNow = new Date();
+      const milliYear = 31557600000
+      //milliMonths = 30.4 days
+      const milliMonth = 2629800000
+
+      let text = ''
+      dateNow = Date.parse(dateNow)
+      //Calculate time since they have been a member in milliseconds
+      let since = dateNow - joined
+      let sinceYears = 0
+      let sinceMonths = 0
+
+      //Find how many years
+      while (since >= milliYear) {
+        sinceYears += 1
+        since -= milliYear
+      }
+
+      //Find how many months
+      while (since >= milliMonth) {
+        sinceMonths += 1
+        since -= milliMonth
+      }
+
+      //Format Text
+      switch (true) {
+        case (sinceYears === 1): text = `${sinceYears} year`
+          break
+        case (sinceYears > 1): text = `${sinceYears} years`
+          break
+      }
+
+      switch (true) {
+        case (text === '' && sinceMonths > 1): text = `${sinceMonths} months`
+          break
+        case (text === '' && sinceMonths === 1): text = `${sinceMonths} month`
+          break
+        case (sinceMonths > 1): text += ` and ${sinceMonths} months`
+          break
+        case (sinceMonths === 1): text += `and ${sinceMonths} month`
+          break
+        case (text === '' && sinceMonths === 0): text = 'Less than 1 month'
+          break
+      }
+      this.dateSinceJoin = text
     }
   },
 
