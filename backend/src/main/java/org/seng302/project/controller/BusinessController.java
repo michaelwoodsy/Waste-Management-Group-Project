9d92c5683436bd9e8f00e5859820f89e831083bf
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-04-08 15:47:42 +1200

    S302T200-50 #time 1h 40m Implemeted all endpoints, with some error checking and testing needing to be done ?implement
    - Created method in Business to remove an administrator
    - Implemeted registering a business, adding Administrators and removing Administrators
    - Changed the getBusiness response so the Businesses administrators businessesAdministered variable is an empty list so the response is not an infinite loop of Business and User objects

--- backend/src/main/java/org/seng302/project/controller/BusinessController.java
+++ backend/src/main/java/org/seng302/project/controller/BusinessController.java
@@ -4,12 +4,15 @@ import net.minidev.json.JSONObject;
 import org.seng302.project.exceptions.*;
 import org.seng302.project.model.Business;
 import org.seng302.project.model.BusinessRepository;
+import org.seng302.project.model.User;
 import org.seng302.project.model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 /**
  * REST controller for handling requests to do with businesses.
@@ -40,10 +43,13 @@ public class BusinessController {
         logger.info("Request to create business");
 
         //If the primary administrator id is not an id of a user
-        if(userRepository.findById(newBusiness.getPrimaryAdministratorId()).isPresent()) {
+        if(userRepository.findById(newBusiness.getPrimaryAdministratorId()).isEmpty()) {
             NoUserExistsException exception = new NoUserExistsException(newBusiness.getPrimaryAdministratorId());
             logger.error(exception.getMessage());
             throw exception;
+        } else {
+            Optional<User> currUser = userRepository.findById(newBusiness.getPrimaryAdministratorId());
+            newBusiness.addAdministrator(currUser.get());
         }
 
         //If business type is not one of the specified business types
@@ -64,6 +70,9 @@ public class BusinessController {
             logger.error(exception.getMessage());
             throw exception;
         }
+
+        businessRepository.save(newBusiness);
+        logger.info(String.format("Successful creation of business %d", newBusiness.getId()));
     }
 
     /**
@@ -78,7 +87,14 @@ public class BusinessController {
 
         logger.info(String.format("Request to get business %d", id));
         try {
-            return businessRepository.findById(id).orElseThrow(() -> new NoBusinessExistsException(id));
+            Business currBusiness = businessRepository.findById(id).orElseThrow(() -> new NoBusinessExistsException(id));
+
+            //Do this so the return is not an infinite loop of businesses and users
+            for (User user: currBusiness.getAdministrators()) {
+                user.setBusinessesAdministered(new ArrayList<>());
+            }
+
+            return currBusiness;
         } catch (NoBusinessExistsException exception) {
             logger.error(exception.getMessage());
             throw exception;
@@ -88,15 +104,24 @@ public class BusinessController {
     /**
      * Adds an individual as an administrator for a business
      * Handles cases that may result in an error
-     * @param id Id of the user to make an administrator
+     * @param id Id of the business to add an administrator to
+     * @param json request body JSON. Contains the id of the user to make an administrator
      */
     @PutMapping("/businesses/{id}/makeAdministrator")
     @ResponseStatus(HttpStatus.OK)
-    public JSONObject addNewAdministrator(@PathVariable int id) {
+    public void addNewAdministrator(@PathVariable int id, @RequestBody JSONObject json) {
         logger.info(String.format("Request to add user with id %d as administrator for business", id));
 
-        //returns empty JSON object for now
-        return new JSONObject();
+        int userId = (int) json.getAsNumber("userId");
+        User currUser = userRepository.findById(userId).orElseThrow(() -> new NoUserExistsException(userId));
+
+
+        Business currBusiness = businessRepository.findById(id).orElseThrow(() -> new NoBusinessExistsException(id));
+        currBusiness.addAdministrator(currUser);
+
+        businessRepository.save(currBusiness);
+
+        logger.info(String.format("Successfully added Administrator %d to business %d", currUser.getId(), currBusiness.getId()));
     }
 
     /**
@@ -106,11 +131,27 @@ public class BusinessController {
      */
     @PutMapping("/businesses/{id}/removeAdministrator")
     @ResponseStatus(HttpStatus.OK)
-    public JSONObject removeAdministrator(@PathVariable int id) {
+    public void removeAdministrator(@PathVariable int id, @RequestBody JSONObject json) {
         logger.info(String.format("Request to remove user with id %d as administrator for business", id));
 
-        //returns empty JSON object for now
-        return new JSONObject();
+        int userId = (int) json.getAsNumber("userId");
+        User currUser = userRepository.findById(userId).orElseThrow(() -> new NoUserExistsException(userId));
+
+
+        Business currBusiness = businessRepository.findById(id).orElseThrow(() -> new NoBusinessExistsException(id));
+
+        //TODO: Make so the primary administrator cant be removed
+        try {
+            currBusiness.removeAdministrator(currUser);
+        } catch (NoUserExistsException exception) {
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+
+
+        businessRepository.save(currBusiness);
+
+        logger.info(String.format("Successfully removed Administrator %d from business %d", currUser.getId(), currBusiness.getId()));
     }
 
 }
