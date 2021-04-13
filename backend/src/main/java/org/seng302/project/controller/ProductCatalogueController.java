7b538fa060762d8e11a195874ad2652f2fb61927
Tom Rizzi <tcr59@uclive.ac.nz>
2021-04-13 22:23:04 +1200

    S302T200-55 #time 30m Implemented getBusinessesProducts method for the ProductCatalogueController ?implement

--- backend/src/main/java/org/seng302/project/controller/ProductCatalogueController.java
+++ backend/src/main/java/org/seng302/project/controller/ProductCatalogueController.java
@@ -1,8 +1,9 @@
 package org.seng302.project.controller;
 
-import org.seng302.project.model.BusinessRepository;
-import org.seng302.project.model.Product;
-import org.seng302.project.model.ProductRepository;
+import org.seng302.project.exceptions.ForbiddenAdministratorActionException;
+import org.seng302.project.exceptions.NoBusinessExistsException;
+import org.seng302.project.exceptions.RequiredFieldsMissingException;
+import org.seng302.project.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Rest controller for products.
@@ -21,19 +24,50 @@ public class ProductCatalogueController {
     private static final Logger logger = LoggerFactory.getLogger(ProductCatalogueController.class.getName());
     private final BusinessRepository businessRepository;
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ProductCatalogueController(BusinessRepository businessRepository, ProductRepository productRepository) {
+    public ProductCatalogueController(
+            BusinessRepository businessRepository,
+            ProductRepository productRepository,
+            UserRepository userRepository) {
         this.businessRepository = businessRepository;
         this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
     /**
      * Gets a list of products that belongs to a business.
-     * @param id ID of the business to get the products of.
+     * @param businessId ID of the business to get the products of.
+     * @param userAuth UsernamePasswordAuthenticationToken from spring security
      * @return List of products that belongs to the business.
      */
-    @GetMapping("/businesses/{id}/products")
-    public List<Product> getBusinessesProducts(@PathVariable int id) { return List.of(); }
+    @GetMapping("/businesses/{businessId}/products")
+    public List<Product> getBusinessesProducts(@PathVariable int businessId, Principal userAuth) {
+        // Get the business
+        Optional<Business> businessResult = businessRepository.findById(businessId);
+
+        // Check if the business exists
+        if (businessResult.isEmpty()) {
+            NoBusinessExistsException exception = new NoBusinessExistsException(businessId);
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+        Business business = businessResult.get();
+
+        // Get the logged in user from the users email
+        String userEmail = userAuth.getName();
+        User loggedInUser = userRepository.findByEmail(userEmail).get(0);
+
+        // Check if the logged in user is the business owner / administrator
+        if (!business.userIsAdmin(loggedInUser.getId())) {
+            ForbiddenAdministratorActionException exception = new ForbiddenAdministratorActionException(businessId);
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+
+        // Get the products for the businesses
+        return productRepository.findAllByBusinessId(businessId);
+    }
 
 }
