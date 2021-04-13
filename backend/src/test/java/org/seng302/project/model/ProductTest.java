d73b0b1f5fc77f18e1018e2c2f92ae39071de06f
Sarah Bealing <smb272@uclive.ac.nz>
2021-04-13 23:38:27 +1200

    S302T200-48 #time 5m Removed businessId field from Product. ProductCatalogue will have a businessId field and will be a list of Products.
    ?fix

--- backend/src/test/java/org/seng302/project/model/ProductTest.java
+++ backend/src/test/java/org/seng302/project/model/ProductTest.java
@@ -19,13 +19,12 @@ public class ProductTest {
     @Test
     public void createTestProduct() {
         Product testProduct = new Product("WATT-420-BEANS", "Watties Baked Beans - 420g can",
-                "Baked Beans as they should be.", 2.2, 70);
+                "Baked Beans as they should be.", 2.2);
 
         Assertions.assertEquals("WATT-420-BEANS", testProduct.getId());
         Assertions.assertEquals("Watties Baked Beans - 420g can", testProduct.getName());
         Assertions.assertEquals("Baked Beans as they should be.", testProduct.getDescription());
         Assertions.assertEquals(2.2, testProduct.getRecommendedRetailPrice());
-        Assertions.assertEquals(70, testProduct.getBusinessId());
         Assertions.assertTrue(testProduct.getCreated().isBefore(LocalDateTime.now()));
         Assertions.assertTrue(testProduct.getCreated().isAfter(LocalDateTime.now().minusSeconds(5)));
     }
