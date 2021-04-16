15aea11567c534c2d48d9da40620fafff1ad4392
Tom Rizzi <tcr59@uclive.ac.nz>
2021-04-17 11:29:03 +1200

    S302T200-60 #time 40m Updated edit product page with checks for product id and other fields. ?implement

--- frontend/src/components/EditProductPage.vue
+++ frontend/src/components/EditProductPage.vue
@@ -1,65 +1,83 @@
 <template>
   <div>
+    <!-- Check if the user is logged in -->
     <login-required
         v-if="!isLoggedIn"
         page="view an individual product"
     />
 
+    <!-- Check if the user is an admin of the business -->
     <admin-required
         v-else-if="!isAdminOfBusiness"
         :page="`of the business ${this.businessId} to edit its products`"
     />
 
-    <div v-else>
-      <!-- Product Id -->
-      <div class="row">
-        <div class="col-6 text-right font-weight-bold">
-          <p>Product Id: </p>
+    <div v-else class="text-center">
+      <!-- Title of the page -->
+      <h1>Edit Product</h1>
+
+      <!-- Display error message if there is one -->
+      <alert v-if="errorMessage">Error: {{ errorMessage }}</alert>
+
+      <!-- Display when the product is loading -->
+      <div v-if="loading">
+        <p class="text-muted">Loading...</p>
+      </div>
+
+      <!-- Edit product div -->
+      <div v-else-if="product">
+
+        <!-- Name -->
+        <div class="row">
+          <div class="col-6 text-right font-weight-bold">
+            <p>Product Id: </p>
+          </div>
+          <div class="col-6">
+            <p>{{ Id }} </p>
+          </div>
         </div>
-        <div class="col-6">
-          <p>{{ Id }} </p>
+
+        <!-- Name -->
+        <div class="row">
+          <div class="col-6 text-right font-weight-bold">
+            <p>Name: </p>
+          </div>
+          <div class="col-6">
+            <p>{{ name }} </p>
+          </div>
+        </div>
+
+        <!-- Description -->
+        <div class="row">
+          <div class="col-6 text-right font-weight-bold">
+            <p>Description: </p>
+          </div>
+          <div class="col-6">
+            <p>{{ description }} </p>
+          </div>
+        </div>
+
+        <!-- Recommended Retail Price -->
+        <div class="row">
+          <div class="col-6 text-right font-weight-bold">
+            <p>Recommended Retail Price: </p>
+          </div>
+          <div class="col-6">
+            <p>{{ recommendedRetailPrice }} </p>
+          </div>
+        </div>
+
+        <!-- Created -->
+        <div class="row">
+          <div class="col-6 text-right font-weight-bold">
+            <p>Created: </p>
+          </div>
+          <div class="col-6">
+            <p>{{ 1 }} </p>
+          </div>
         </div>
       </div>
 
-      <!-- Name -->
-      <div class="row">
-        <div class="col-6 text-right font-weight-bold">
-          <p>Name: </p>
-        </div>
-        <div class="col-6">
-          <p>{{ name }} </p>
-        </div>
-      </div>
-
-      <!-- Description -->
-      <div class="row">
-        <div class="col-6 text-right font-weight-bold">
-          <p>Description: </p>
-        </div>
-        <div class="col-6">
-          <p>{{ description }} </p>
-        </div>
-      </div>
-
-      <!-- Recommended Retail Price -->
-      <div class="row">
-        <div class="col-6 text-right font-weight-bold">
-          <p>Recommended Retail Price: </p>
-        </div>
-        <div class="col-6">
-          <p>{{ recommendedRetailPrice }} </p>
-        </div>
-      </div>
-
-      <!-- Created -->
-      <div class="row">
-        <div class="col-6 text-right font-weight-bold">
-          <p>Created: </p>
-        </div>
-        <div class="col-6">
-          <p>{{ created }} </p>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -67,12 +85,26 @@
 <script>
 import LoginRequired from "@/components/LoginRequired";
 import AdminRequired from "@/components/AdminRequired";
+import Alert from "@/components/Alert";
+// import {Business} from "@/Api";
 
 export default {
   name: "EditProductPage",
+  mounted () {
+    this.loadProduct();
+  },
+  data () {
+    return {
+      errorMessage: null,
+      loading: true,
+      product: null,
+      newProduct: {}
+    }
+  },
   components: {
     LoginRequired,
-    AdminRequired
+    AdminRequired,
+    Alert
   },
   computed: {
     /**
@@ -122,6 +154,48 @@ export default {
         }
       })
       return isAdmin
+    }
+  },
+  methods: {
+    loadProduct () {
+      // Uncomment when a getProducts method exists
+
+      // Business.getProducts(this.businessId)
+      //     .then((res) => {
+      //       this.product = res.data.find(prod => prod.id === this.productId.toString())
+      //       if (!this.product) {
+      //         this.errorMessage = `There is no product with id ${this.productId}.`
+      //       }
+      //       this.loading = false
+      //     })
+      //     .catch((err) => {
+      //       this.errorMessage = err.response.data.message;
+      //       this.loading = false
+      //     })
+
+      // Fake data for testing here
+      const data = [
+        {
+          "id": "WATT-420-BEANS",
+          "name": "Watties Baked Beans - 420g can",
+          "description": "Baked Beans as they should be.",
+          "manufacturer": "Heinz Wattie's Limited",
+          "recommendedRetailPrice": 2.2,
+          "created": "2021-04-16T23:08:48.584Z",
+          "images": [
+            {
+              "id": 1234,
+              "filename": "/media/images/23987192387509-123908794328.png",
+              "thumbnailFilename": "/media/images/23987192387509-123908794328_thumbnail.png"
+            }
+          ]
+        }
+      ]
+      this.product = data.find(prod => prod.id === this.productId.toString())
+      if (!this.product) {
+        this.errorMessage = `There is no product with id ${this.productId}.`
+      }
+      this.loading = false
     }
   }
 }
