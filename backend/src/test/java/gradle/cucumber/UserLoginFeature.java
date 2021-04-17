87ff5f896466258e80a03177f6acac52225a6a55
Sarah Bealing <smb272@uclive.ac.nz>
2021-04-17 17:42:54 +1200

    S302T200-117 #time 10m More manual testing of Cucumber setup. Documented manual testing on wiki.
    ?testmanual ?document

--- backend/src/test/java/gradle/cucumber/UserLoginFeature.java
+++ backend/src/test/java/gradle/cucumber/UserLoginFeature.java
@@ -5,6 +5,24 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class UserLoginFeature {
+    @Given("An account exists with the email {string}")
+    public void an_account_exists_with_the_email(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I try to register with the email {string}")
+    public void i_try_to_register_with_the_email(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The registration is unsuccessful")
+    public void the_registration_is_unsuccessful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
     @Given("I have created an account with the username {string} and the password {string}")
     public void i_have_created_an_account_with_the_username_and_the_password(String username, String password) {
--- backend/src/test/resources/gradle.cucumber/user.feature
+++ backend/src/test/resources/gradle.cucumber/user.feature
@@ -1,5 +1,11 @@
 Feature: U1 - Registering and logging into an individual account
 
+  Scenario: AC2 - If I try to register an account with an email address that is already registered, the system should not create the account but let me know
+    Given An account exists with the email "sarahbear77@gmail.com"
+    When I try to register with the email "sarahbear77@gmail.com"
+    Then The registration is unsuccessful
+
+
   Scenario: AC12 - I can log back into my profile with my username (email address) and password.
     Given I have created an account with the username "sarahbear77@gmail.com" and the password "password"
     When I log back into my profile with the username "sarahbear77@gmail.com" and the password "password"
