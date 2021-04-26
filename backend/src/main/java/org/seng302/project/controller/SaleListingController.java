4889625ba27a9ad0eadd126236808cc898d148b9
Tom Rizzi <tcr59@uclive.ac.nz>
2021-04-26 13:29:34 +1200

    S302T200-106 #time 1h Implemented getBusinessesListings method in SaleListingController. ?implement
    - Implemented get method for business/{id}/listings in controller.
    - Updated constructor and InventoryItem relationship in SaleListing class.

--- backend/src/main/java/org/seng302/project/controller/SaleListingController.java
+++ backend/src/main/java/org/seng302/project/controller/SaleListingController.java
@@ -1,6 +1,9 @@
 package org.seng302.project.controller;
 
 import org.seng302.project.controller.authentication.AppUserDetails;
+import org.seng302.project.exceptions.ForbiddenAdministratorActionException;
+import org.seng302.project.exceptions.NoBusinessExistsException;
+import org.seng302.project.exceptions.UserNotAdministratorException;
 import org.seng302.project.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Rest controller for sale listings.
@@ -20,16 +24,16 @@ public class SaleListingController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductCatalogueController.class.getName());
     private final BusinessRepository businessRepository;
-    private final InventoryItemRepository inventoryItemRepository;
+    private final SaleListingRepository saleListingRepository;
     private final UserRepository userRepository;
 
     @Autowired
     public SaleListingController(
             BusinessRepository businessRepository,
-            InventoryItemRepository inventoryItemRepository,
+            SaleListingRepository saleListingRepository,
             UserRepository userRepository) {
         this.businessRepository = businessRepository;
-        this.inventoryItemRepository = inventoryItemRepository;
+        this.saleListingRepository = saleListingRepository;
         this.userRepository = userRepository;
     }
 
@@ -44,6 +48,42 @@ public class SaleListingController {
     }
 
     /**
+     * Gets a business with the provided id.
+     * @param businessId The id of the business to look for.
+     * @return The business with the corresponding id.
+     * @throws NoBusinessExistsException Thrown if the business doesn't exist.
+     */
+    private Business getBusiness(Integer businessId) throws NoBusinessExistsException {
+        // Get business from repository
+        Optional<Business> foundBusiness = businessRepository.findById(businessId);
+
+        // Check if the business exists
+        if (foundBusiness.isEmpty()) {
+            NoBusinessExistsException exception = new NoBusinessExistsException(businessId);
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+
+        // Return the found business
+        return foundBusiness.get();
+    }
+
+    /**
+     * Checks if the user is the owner or administrator of the business. Throws an exception if they aren't
+     * @param user The user to check.
+     * @param business The business to check.
+     * @throws ForbiddenAdministratorActionException Thrown if the user isn't and owner or admin of the business.
+     */
+    private void checkUserIsAdminOfBusiness(User user, Business business) throws ForbiddenAdministratorActionException {
+        // Check if the logged in user is the business owner / administrator
+        if (!(business.userIsAdmin(user.getId()) || business.getPrimaryAdministratorId().equals(user.getId()))) {
+            ForbiddenAdministratorActionException exception = new ForbiddenAdministratorActionException(business.getId());
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+    }
+
+    /**
      * Gets a list of sale listings for a business.
      * @param businessId Business to get the sale listings from.
      * @param appUser The user that made the request.
@@ -53,8 +93,21 @@ public class SaleListingController {
     public List<SaleListing> getBusinessesListings(
             @PathVariable int businessId,
             @AuthenticationPrincipal AppUserDetails appUser) {
-        logger.info(getLoggedInUser(appUser).getEmail());
-        return List.of();
+
+        // Get the user that made the request
+        User user = getLoggedInUser(appUser);
+
+        logger.info("User with id " + user.getId() +
+                " trying to get sale listings of business with id " + businessId  + ".");
+
+        // Get the business of the request
+        Business business = getBusiness(businessId);
+
+        // Check the user is an admin of the business
+        checkUserIsAdminOfBusiness(user, business);
+
+        // Get the sale listings of the business
+        return saleListingRepository.findAllByBusinessId(businessId);
     }
 
 }
