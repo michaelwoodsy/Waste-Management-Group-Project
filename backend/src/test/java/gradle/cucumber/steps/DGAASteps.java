500979bc6f3b3eb044ac53037d10aeb980cd2ddf
Sarah Bealing <smb272@uclive.ac.nz>
2021-05-13 16:05:15 +1200

    S302T200-11 #time 30m Attempt to get first Cucumber test working... not much luck
    ?test

--- backend/src/test/java/gradle/cucumber/steps/DGAASteps.java
+++ backend/src/test/java/gradle/cucumber/steps/DGAASteps.java
@@ -1,152 +1,164 @@
 package gradle.cucumber.steps;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockito.Mockito;
+import org.seng302.project.DGAAChecker;
+import org.springframework.boot.SpringApplication;
+
 
 public class DGAASteps {
 
+    public DGAAChecker mockDGAAChecker;
+    public SpringApplication mockSpringApplication;
+
+    @Before
+    public void setup() {
+        mockDGAAChecker = Mockito.mock(DGAAChecker.class);
+        mockSpringApplication = Mockito.mock(SpringApplication.class);
+    }
+
     //AC1
+    @Given("The application is running")
+    public void the_application_is_running() {
+        mockSpringApplication.run();
+    }
 
     @When("The application is \\(re-) started")
     public void the_application_is_re_started() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        mockSpringApplication.run();
     }
 
     @Then("A check is run for the existence of the DGAA")
     public void a_check_is_run_for_the_existence_of_the_dgaa() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //TODO: Actually, there were zero interactions with this mock.
+        // Even when this line was put in above method
+        Mockito.verify(mockDGAAChecker, Mockito.times(1)).dgaaExists();
     }
 
 
-    //AC2
-
-    @Given("The application is running")
-    public void the_application_is_running() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("A period of time has passed")
-    public void a_period_of_time_has_passed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    //AC3
-
-    @Given("A DGAA does not exist")
-    public void a_dgaa_does_not_exist() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("The existence of the DGAA is checked for")
-    public void the_existence_of_the_dgaa_is_checked_for() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("A DGAA is created automatically")
-    public void a_dgaa_is_created_automatically() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("An entry is added into the error log")
-    public void an_entry_is_added_into_the_error_log() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("The DGAA has predefined username {string} and password {string}")
-    public void the_dgaa_has_predefined_username_and_password(String username, String password) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("The DGAA username and password are changed to {string} and {string}")
-    public void the_dgaa_username_and_password_are_changed_to_and(String newUsername, String newPassword) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("The DGAA username and password are changed in the database")
-    public void the_dgaa_username_and_password_are_changed_in_the_database() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    //AC4
-
-    @Given("There exists a user with the first name {string} and last name {string}")
-    public void there_exists_a_user_with_the_first_name_and_last_name(String fName, String lName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("The DGAA searches with the search string {string}")
-    public void the_dgaa_searches_with_the_search_string(String searchString) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("The user with the first name {string} and last name {string} is returned to the DGAA")
-    public void the_user_with_the_first_name_and_last_name_is_returned_to_the_dgaa(String fName, String lName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    //AC6
-
-    @When("The DGAA assigns admin rights to the user with the first name {string} and last name {string}")
-    public void the_dgaa_assigns_admin_rights_to_the_user_with_the_first_name_and_last_name(String fName, String lName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("The role of the user is updated to GAA")
-    public void the_role_of_the_user_is_updated_to_gaa() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    //AC7
-
-    @Given("There exists a user with a role of GAA")
-    public void there_exists_a_user_with_a_role_of_gaa() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("The DGAA removes admin rights from the user")
-    public void the_dgaa_removes_admin_rights_from_the_user() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("The role of the user is updated to user")
-    public void the_role_of_the_user_is_updated_to_user() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    //AC8
-
-    @Given("There exists a business with the name {string}")
-    public void there_exists_a_business_with_the_name(String businessName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("A DGAA assigns admin rights to the business")
-    public void a_dgaa_assigns_admin_rights_to_the_business() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("An exception is thrown")
-    public void an_exception_is_thrown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+//    //AC2
+//
+//    @When("A period of time has passed")
+//    public void a_period_of_time_has_passed() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC3
+//
+//    @Given("A DGAA does not exist")
+//    public void a_dgaa_does_not_exist() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("The existence of the DGAA is checked for")
+//    public void the_existence_of_the_dgaa_is_checked_for() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("A DGAA is created automatically")
+//    public void a_dgaa_is_created_automatically() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("An entry is added into the error log")
+//    public void an_entry_is_added_into_the_error_log() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Given("The DGAA has predefined username {string} and password {string}")
+//    public void the_dgaa_has_predefined_username_and_password(String username, String password) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("The DGAA username and password are changed to {string} and {string}")
+//    public void the_dgaa_username_and_password_are_changed_to_and(String newUsername, String newPassword) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("The DGAA username and password are changed in the database")
+//    public void the_dgaa_username_and_password_are_changed_in_the_database() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC4
+//
+//    @Given("There exists a user with the first name {string} and last name {string}")
+//    public void there_exists_a_user_with_the_first_name_and_last_name(String fName, String lName) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("The DGAA searches with the search string {string}")
+//    public void the_dgaa_searches_with_the_search_string(String searchString) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("The user with the first name {string} and last name {string} is returned to the DGAA")
+//    public void the_user_with_the_first_name_and_last_name_is_returned_to_the_dgaa(String fName, String lName) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC6
+//
+//    @When("The DGAA assigns admin rights to the user with the first name {string} and last name {string}")
+//    public void the_dgaa_assigns_admin_rights_to_the_user_with_the_first_name_and_last_name(String fName, String lName) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("The role of the user is updated to GAA")
+//    public void the_role_of_the_user_is_updated_to_gaa() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC7
+//
+//    @Given("There exists a user with a role of GAA")
+//    public void there_exists_a_user_with_a_role_of_gaa() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("The DGAA removes admin rights from the user")
+//    public void the_dgaa_removes_admin_rights_from_the_user() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("The role of the user is updated to user")
+//    public void the_role_of_the_user_is_updated_to_user() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC8
+//
+//    @Given("There exists a business with the name {string}")
+//    public void there_exists_a_business_with_the_name(String businessName) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("A DGAA assigns admin rights to the business")
+//    public void a_dgaa_assigns_admin_rights_to_the_business() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("An exception is thrown")
+//    public void an_exception_is_thrown() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
 
 }
--- backend/src/test/resources/gradle.cucumber/dgaa.feature
+++ backend/src/test/resources/gradle.cucumber/dgaa.feature
@@ -6,43 +6,43 @@ Feature: Default Global Application Admin (U4)
     Then A check is run for the existence of the DGAA
 
 
-  Scenario: AC2 - The application periodically checks that at least one DGAA exists.
-    Given The application is running
-    When A period of time has passed
-    Then A check is run for the existence of the DGAA
-
-
-  Scenario: AC3 - If a DGAA does not exist (e.g. it was incorrectly deleted directly from the database),
-  the DGAA should be created automatically and an entry should be made into the error log.
-    Given A DGAA does not exist
-    When The existence of the DGAA is checked for
-    Then A DGAA is created automatically
-    And An entry is added into the error log
-
-
-  Scenario: AC3 - The DGAA will have a predefined username and password. Changing this username and password would
-  require access to the server itself (e.g. changing it in the database).
-    Given The DGAA has predefined username "dgaa@resale.com" and password "1AmTheDGAA"
-    When The DGAA username and password are changed to "admin@resale.com" and "1AmTheAdmin"
-    Then The DGAA username and password are changed in the database
-
-  Scenario: AC4 - The DGAA can search for a particular individual.
-    Given There exists a user with the first name "Bob" and last name "Bean"
-    When The DGAA searches with the search string "Bob Bean"
-    Then The user with the first name "Bob" and last name "Bean" is returned to the DGAA
-
-  Scenario: AC6 - The DGAA can make the individual a GAA.
-    Given There exists a user with the first name "Bob" and last name "Bean"
-    When The DGAA assigns admin rights to the user with the first name "Bob" and last name "Bean"
-    Then The role of the user is updated to GAA
-
-  Scenario: AC7 - The DGAA can remove admin rights from any individual.
-    Given There exists a user with a role of GAA
-    When The DGAA removes admin rights from the user
-    Then The role of the user is updated to user
-
-  Scenario: AC8 - Only individuals can be given application admin rights.
-  Business accounts cannot be given admin rights.
-    Given There exists a business with the name "Bob's Beans"
-    When A DGAA assigns admin rights to the business
-    Then An exception is thrown
+#  Scenario: AC2 - The application periodically checks that at least one DGAA exists.
+#    Given The application is running
+#    When A period of time has passed
+#    Then A check is run for the existence of the DGAA
+#
+#
+#  Scenario: AC3 - If a DGAA does not exist (e.g. it was incorrectly deleted directly from the database),
+#  the DGAA should be created automatically and an entry should be made into the error log.
+#    Given A DGAA does not exist
+#    When The existence of the DGAA is checked for
+#    Then A DGAA is created automatically
+#    And An entry is added into the error log
+#
+#
+#  Scenario: AC3 - The DGAA will have a predefined username and password. Changing this username and password would
+#  require access to the server itself (e.g. changing it in the database).
+#    Given The DGAA has predefined username "dgaa@resale.com" and password "1AmTheDGAA"
+#    When The DGAA username and password are changed to "admin@resale.com" and "1AmTheAdmin"
+#    Then The DGAA username and password are changed in the database
+#
+#  Scenario: AC4 - The DGAA can search for a particular individual.
+#    Given There exists a user with the first name "Bob" and last name "Bean"
+#    When The DGAA searches with the search string "Bob Bean"
+#    Then The user with the first name "Bob" and last name "Bean" is returned to the DGAA
+#
+#  Scenario: AC6 - The DGAA can make the individual a GAA.
+#    Given There exists a user with the first name "Bob" and last name "Bean"
+#    When The DGAA assigns admin rights to the user with the first name "Bob" and last name "Bean"
+#    Then The role of the user is updated to GAA
+#
+#  Scenario: AC7 - The DGAA can remove admin rights from any individual.
+#    Given There exists a user with a role of GAA
+#    When The DGAA removes admin rights from the user
+#    Then The role of the user is updated to user
+#
+#  Scenario: AC8 - Only individuals can be given application admin rights.
+#  Business accounts cannot be given admin rights.
+#    Given There exists a business with the name "Bob's Beans"
+#    When A DGAA assigns admin rights to the business
+#    Then An exception is thrown
