8d9f1ca4544cc8a1e099897b36aeb729501aaa72
Sarah Bealing <smb272@uclive.ac.nz>
2021-05-14 12:25:13 +1200

    S302T200-11 #time 10m Fixed DGAATest to use jupiter so that the tests run.
    ?fix

--- backend/src/test/java/org/seng302/project/model/DGAATest.java
+++ backend/src/test/java/org/seng302/project/model/DGAATest.java
@@ -1,6 +1,5 @@
 package org.seng302.project.model;
 
-import org.junit.Test;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
