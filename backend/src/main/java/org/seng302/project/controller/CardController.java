9bc242f14497b2e83002d36732b20ef6094a3150
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-05-18 19:55:38 +1200

    S302T200-153 #time 1h finished implementing get method for single card. Wrote junit tests ?implement ?test
    - Created Test class for Card Controller and included 3 tests, for retrieving a card when not logged in, when retrieving a card that does not exist, and when retrieving an actual card

--- backend/src/main/java/org/seng302/project/controller/CardController.java
+++ backend/src/main/java/org/seng302/project/controller/CardController.java
@@ -1,5 +1,13 @@
 package org.seng302.project.controller;
 
+import org.seng302.project.exceptions.*;
+import org.seng302.project.exceptions.NoCardExistsException;
+import org.seng302.project.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,5 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CardController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CardController.class.getName());
+    private final CardRepository cardRepository;
 
+
+    @Autowired
+    public CardController(
+            CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+
+    /**
+     * Gets a single card with id cardID.
+     * @param id of the card to retrieve.
+     * @return a single card object.
+     */
+    @GetMapping("/cards/{id}")
+    public Card getBusiness(@PathVariable int id) {
+
+        logger.info(String.format("Request to get card with id %d", id));
+        try {
+            return cardRepository.findById(id).orElseThrow(() -> new NoCardExistsException(id));
+        } catch (NoCardExistsException noCardExistsException) {
+            logger.warn(noCardExistsException.getMessage());
+            throw noCardExistsException;
+        } catch (Exception exception) {
+            logger.error(String.format("Unexpected error while getting card: %s", exception.getMessage()));
+            throw exception;
+        }
+    }
 }
