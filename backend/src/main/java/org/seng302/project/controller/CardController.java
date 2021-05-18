e6468f19ae033e4ad66f3b73099dc1834418ab61
Sarah Bealing <smb272@uclive.ac.nz>
2021-05-18 18:55:00 +1200

    S302T200-167 #time 40m Reviewed the card model and repository. Removed unused imports. AuthenticationTest class was failing when trying to clear the user repository so I added a line to clear the card repository first. We may have some problems with the testRepository() in CardTest when there are actual cards in the repository so I tried mocking the card repository but didn't have any luck so left a TODO.
    ?review ?test ?fix

--- backend/src/main/java/org/seng302/project/controller/CardController.java
+++ backend/src/main/java/org/seng302/project/controller/CardController.java
@@ -1,19 +1,7 @@
 package org.seng302.project.controller;
 
-import net.minidev.json.JSONObject;
-import org.seng302.project.exceptions.NoUserExistsException;
-import org.seng302.project.exceptions.RequiredFieldsMissingException;
-import org.seng302.project.model.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 /**
  * REST controller for handling requests to do with marketplace cards.
