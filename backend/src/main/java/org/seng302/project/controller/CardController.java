3a470f5602500556b81968fa4b38bdf550edb368
Sarah Bealing <smb272@uclive.ac.nz>
2021-05-18 19:24:06 +1200

    S302T200-151 #time 5m
    Put imports back in that were removed on dev. Left TODO for Lewis.
    
    ?fix

--- backend/src/main/java/org/seng302/project/controller/CardController.java
+++ backend/src/main/java/org/seng302/project/controller/CardController.java
@@ -1,5 +1,17 @@
 package org.seng302.project.controller;
 
+import net.minidev.json.JSONObject;
+import org.seng302.project.model.Card;
+import org.seng302.project.model.CardRepository;
+import org.seng302.project.model.User;
+import org.seng302.project.model.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -25,6 +37,8 @@ public class CardController {
         logger.info("Request to create card");
 
         Integer creatorId = (Integer) json.getAsNumber("creatorId");
+
+        //TODO: handle case when user is not found.
         User creator = userRepository.findById(creatorId).get();
 
         String section = json.getAsString("section");
