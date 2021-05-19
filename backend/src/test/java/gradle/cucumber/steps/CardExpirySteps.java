343740874b791b3baba33ca93e204eae0d1611e2
Sarah Bealing <smb272@uclive.ac.nz>
2021-05-20 11:18:23 +1200

    S302T200-157 #time 15m
    Finished writing Cucumber scenarios based on AC's for UCM4. Generated method stubs for their step definitions.
    
    ?test

---
+++ backend/src/test/java/gradle/cucumber/steps/CardExpirySteps.java
@@ -0,0 +1,78 @@
+package gradle.cucumber.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+/**
+ * Cucumber steps for UCM4 - Card expiry
+ */
+public class CardExpirySteps {
+
+//    //AC1
+//
+//    @Given("A user has created a card")
+//    public void a_user_has_created_a_card() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("That user tries to delete their card")
+//    public void that_user_tries_to_delete_their_card() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("The card is successfully deleted")
+//    public void the_card_is_successfully_deleted() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("A different user tries to delete their card")
+//    public void a_different_user_tries_to_delete_their_card() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("An appropriate exception is thrown")
+//    public void an_appropriate_exception_is_thrown() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("The card is not deleted")
+//    public void the_card_is_not_deleted() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC2
+//
+//    @Given("A system administrator exists")
+//    public void a_system_administrator_exists() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("The system administrator tries to delete the card")
+//    public void the_system_administrator_tries_to_delete_the_card() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    //AC4
+//
+//    @When("The card has an expiry date of more than {int} hours ago")
+//    public void the_card_has_an_expiry_date_of_more_than_hours_ago(Integer noActionLimit) {
+//        //noActionLimit = 24 (hours)
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("The card is automatically deleted")
+//    public void the_card_is_automatically_deleted() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+}
--- backend/src/test/resources/gradle.cucumber/cardExpiry.feature
+++ backend/src/test/resources/gradle.cucumber/cardExpiry.feature
@@ -1,18 +1,25 @@
 Feature: UCM4 - Card expiry
 
-  Scenario: AC1: I can delete any card that I created but I cannot delete a card created by another user.
-    Given
-    When
-    Then
-
-  Scenario: AC2: A system administrator can delete any cards.
-    Given
-    When
-    Then
-
-  Scenario: AC4: If I take no action within 24 hours of the notification (card expiry) then the card will be deleted automatically
-    Given
-    When
-    Then
+#  Scenario: AC1: I can delete any card that I created but I cannot delete a card created by another user.
+#    Given A user has created a card
+#    When That user tries to delete their card
+#    Then The card is successfully deleted
+#
+#  Scenario: AC1: I can delete any card that I created but I cannot delete a card created by another user.
+#    Given A user has created a card
+#    When A different user tries to delete their card
+#    Then An appropriate exception is thrown
+#    And The card is not deleted
+#
+#  Scenario: AC2: A system administrator can delete any cards.
+#    Given A system administrator exists
+#    And A user has created a card
+#    When The system administrator tries to delete the card
+#    Then The card is successfully deleted
+#
+#  Scenario: AC4: If I take no action within 24 hours of the notification (card expiry) then the card will be deleted automatically
+#    Given A user has created a card
+#    When The card has an expiry date of more than 24 hours ago
+#    Then The card is automatically deleted
 
 
