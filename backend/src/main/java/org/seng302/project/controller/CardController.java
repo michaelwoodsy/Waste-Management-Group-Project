b0c9b0a3caa643767a57bd9d7cb138b349c39481
Tom Rizzi <tcr59@uclive.ac.nz>
2021-05-19 20:47:04 +1200

    S302T200-169 #time 1h 30m Merged two CardController unit tests, updating the current ones to use mocking. ?test
    - Took very long as I kept running into errors with initialising the test class :'(

--- backend/src/main/java/org/seng302/project/controller/CardController.java
+++ backend/src/main/java/org/seng302/project/controller/CardController.java
@@ -25,19 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CardController {
 
-    /**
-     * Endpoint for getting all cards in a section.
-     *
-     * @param section Section to get all cards from.
-     * @param appUser The user that made the request.
-     * @return List of Cards in the corresponding section.
-     */
-    @GetMapping("/cards")
-    public List<Card> getAllCards(
-            @RequestParam int section,
-            @AuthenticationPrincipal AppUserDetails appUser) {
-        return List.of();
-    }
+
     private static final Logger logger = LoggerFactory.getLogger(CardController.class.getName());
     private final CardRepository cardRepository;
 
@@ -69,4 +57,18 @@ public class CardController {
             throw exception;
         }
     }
+
+    /**
+     * Endpoint for getting all cards in a section.
+     *
+     * @param section Section to get all cards from.
+     * @param appUser The user that made the request.
+     * @return List of Cards in the corresponding section.
+     */
+//    @GetMapping("/cards")
+//    public List<Card> getAllCards(
+//            @RequestParam int section,
+//            @AuthenticationPrincipal AppUserDetails appUser) {
+//        return List.of();
+//    }
 }
