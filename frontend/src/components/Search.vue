1996aa88c40d85f77e190bde2a6f24395a088f4e
Sarah Bealing <smb272@uclive.ac.nz>
2021-06-15 12:21:26 +1200

    S302T200-221 #time 1h Implemented a search page that lets you tab between searching users and searching businesses.
    ?implement

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,94 @@
+<!--
+Search.vue
+Page for searching users or businesses
+-->
+
+<template>
+  <page-wrapper>
+    <!-- Check the user is logged in -->
+    <login-required
+        v-if="!isLoggedIn"
+        page="search for Users or Businesses"
+    />
+
+    <div v-else>
+      <br>
+      <!--    Div for search tabs    -->
+      <div class="row justify-content-center">
+        <div class="col"/>
+        <div class="col-12">
+          <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+              <a id="for-sale-link"
+                 :class="{'nav-link': true, 'active': this.tabSelected === 'Users'}"
+                 class="pointer"
+                 @click="changeTab('Users')">
+                Users
+              </a>
+            </li>
+            <li class="nav-item">
+              <a id="wanted-link"
+                 :class="{'nav-link': true, 'active': this.tabSelected === 'Businesses'}"
+                 class="pointer"
+                 @click="changeTab('Businesses')">
+                Businesses
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <UserSearch v-if="tabSelected === 'Users'"></UserSearch>
+      <BusinessSearch v-if="tabSelected === 'Businesses'"></BusinessSearch>
+
+    </div>
+
+  </page-wrapper>
+</template>
+
+<script>
+import UserSearch from "@/components/user/UserSearch";
+import BusinessSearch from "@/components/business/BusinessSearch";
+import LoginRequired from "@/components/LoginRequired";
+import PageWrapper from "@/components/PageWrapper";
+export default {
+  name: "Search.vue",
+  components: {
+    PageWrapper,
+    LoginRequired,
+    BusinessSearch,
+    UserSearch
+  },
+  data() {
+    return {
+      tabSelected: 'Users', //Default tab
+    }
+  },
+
+  computed: {
+    /**
+     * Check if user is logged in
+     * @returns {boolean|*}
+     */
+    isLoggedIn() {
+      return this.$root.$data.user.state.loggedIn
+    }
+  },
+
+  methods: {
+    /**
+     *
+     * @param tab what to search through e.g.
+     * 'Users' or 'Businesses'
+     */
+    changeTab(tab) {
+      this.tabSelected = tab;
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/business/BusinessSearch.vue
@@ -0,0 +1,121 @@
+<!--
+BusinessSearch.vue
+Component on Search page for searching businesses
+-->
+
+
+<template>
+  <page-wrapper>
+
+    <div v-if="isLoggedIn" class="container-fluid">
+
+      <!--    Search Businesses Header    -->
+      <div class="row">
+        <div class="col-12 text-center mb-2">
+          <h4>Search Businesses</h4>
+        </div>
+      </div>
+
+      <!--    Error Alert    -->
+      <div v-if="error" class="row">
+        <div class="col-8 offset-2 text-center mb-2">
+          <alert>{{ error }}</alert>
+        </div>
+      </div>
+
+      <!--    Search Input    -->
+      <div class="row mb-2">
+        <div class="col-12 col-sm-8 col-lg-6 col-xl-4 col-centered">
+          <div class="input-group">
+            <input id="search"
+                   v-model="searchTerm"
+                   class="form-control no-outline"
+                   placeholder="business name"
+                   type="search"
+                   @keyup.enter="search">
+            <div class="input-group-append">
+              <button class="btn btn-primary no-outline" type="button" @click="search">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!--    Result Information    -->
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <div class="text-center">
+            <showing-results-text
+                :items-per-page="resultsPerPage"
+                :page="page"
+                :total-count="totalCount"
+            />
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+    </div>
+  </page-wrapper>
+</template>
+
+<script>
+import ShowingResultsText from "@/components/ShowingResultsText";
+import PageWrapper from "@/components/PageWrapper";
+import Alert from "@/components/Alert";
+
+export default {
+  name: "BusinessSearch",
+  components: {
+    PageWrapper,
+    ShowingResultsText,
+    Alert,
+  },
+  data() {
+    return {
+      searchTerm: "",
+      businesses: [],
+      error: null,
+      orderCol: null,
+      orderDirection: false, // False -> Ascending
+      resultsPerPage: 10,
+      page: 1,
+      loading: false
+    }
+  },
+  computed: {
+    /**
+     * Check if user is logged in
+     * @returns {boolean|*}
+     */
+    isLoggedIn() {
+      return this.$root.$data.user.state.loggedIn
+    },
+
+    /**
+     * Calculates the number of results in businesses array
+     * @returns {number}
+     */
+    totalCount() {
+      return this.businesses.length
+    },
+  },
+  methods: {
+    search() {
+      //TODO: implement me
+    }
+  }
+}
+</script>
+
+<style scoped>
+.col-centered {
+  margin: 0 auto;
+  float: none;
+}
+
+</style>
