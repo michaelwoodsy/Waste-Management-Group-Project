8690a77078b852f2a9a0e544cf28ddcf65f3ef4a
Oliver Chaplin <ojc30@uclive.ac.nz>
2021-07-01 15:01:18 +1200

    S302T200-233 #time 30m Implemented service method for adding a new keyword. Implemented check for existing keywords. Wrote unit test for trying to add a new keyword that already exists. ?implement ?test

--- backend/src/main/java/org/seng302/project/serviceLayer/service/KeywordService.java
+++ backend/src/main/java/org/seng302/project/serviceLayer/service/KeywordService.java
@@ -1,11 +1,15 @@
 package org.seng302.project.serviceLayer.service;
 
+import org.seng302.project.repositoryLayer.model.Keyword;
 import org.seng302.project.repositoryLayer.repository.KeywordRepository;
 import org.seng302.project.serviceLayer.dto.keyword.AddKeywordResponseDTO;
+import org.seng302.project.serviceLayer.exceptions.keyword.KeywordExistsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Service class with methods for handling Keywords
@@ -28,7 +32,16 @@ public class KeywordService {
      * @return an object containing the ID of the keyword that has been added.
      */
     public AddKeywordResponseDTO addKeyword(String name) {
-        return null;
+        List<Keyword> keywords = keywordRepository.findByName(name);
+        if (!keywords.isEmpty()) {
+            var exception = new KeywordExistsException(name);
+            logger.error(exception.getMessage());
+            throw exception;
+        }
+
+        var keyword = new Keyword(name);
+        keyword = keywordRepository.save(keyword);
+        return new AddKeywordResponseDTO(keyword.getId());
     }
 
 }
--- backend/src/test/java/org/seng302/project/serviceLayer/service/KeywordServiceTest.java
+++ backend/src/test/java/org/seng302/project/serviceLayer/service/KeywordServiceTest.java
@@ -1,17 +1,20 @@
 package org.seng302.project.serviceLayer.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.seng302.project.repositoryLayer.model.Keyword;
 import org.seng302.project.repositoryLayer.repository.KeywordRepository;
 import org.seng302.project.serviceLayer.dto.keyword.AddKeywordDTO;
 import org.seng302.project.serviceLayer.dto.keyword.AddKeywordResponseDTO;
+import org.seng302.project.serviceLayer.exceptions.keyword.KeywordExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -20,29 +23,55 @@ import java.util.List;
 @SpringBootTest
 class KeywordServiceTest {
 
-    private final List<Keyword> keywords = new ArrayList<>();
     @Autowired
     private KeywordService keywordService;
     @MockBean
     private KeywordRepository keywordRepository;
+    private List<Keyword> keywords;
+
+    @BeforeEach
+    void setup() {
+        keywords = new ArrayList<>();
+    }
 
     /**
-     * Tests that adding a keyword successfully adds it to the repository and returns a response DTO.
+     * Tests that adding a new keyword successfully adds it to the repository and returns a response DTO.
      */
     @Test
-    void testAddKeyword() {
+    void addKeyword_newKeyword_success() {
+        Mockito.when(keywordRepository.findByName(Mockito.any(String.class)))
+                .thenReturn(Collections.emptyList());
+
         Mockito.when(keywordRepository.save(Mockito.any(Keyword.class)))
                 .thenAnswer(invocation -> {
                     Keyword keyword = invocation.getArgument(0);
                     keyword.setId(keywords.size() + 1);
                     keywords.add(keyword);
-                    return null;
+                    return keyword;
                 });
 
         AddKeywordDTO dto = new AddKeywordDTO("TestKeyword");
         AddKeywordResponseDTO responseDTO = keywordService.addKeyword(dto.getName());
         Assertions.assertEquals(1, responseDTO.getKeywordId());
         Assertions.assertEquals(1, keywords.size());
+    }
+
+    /**
+     * Tests that attempting to add a keyword that already exists results in an error.
+     */
+    @Test
+    void addKeyword_alreadyExists_throwsException() {
+        Mockito.when(keywordRepository.findByName(Mockito.any(String.class)))
+                .thenAnswer(invocation -> {
+                    String name = invocation.getArgument(0);
+                    Keyword keyword = new Keyword(name);
+                    keyword.setId(1);
+                    return List.of(keyword);
+                });
+
+        AddKeywordDTO dto = new AddKeywordDTO("TestKeyword");
+        Assertions.assertThrows(KeywordExistsException.class,
+                () -> keywordService.addKeyword(dto.getName()));
     }
 
 }
