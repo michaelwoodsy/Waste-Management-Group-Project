38c4d66abecd2a7d6d5d595fa22c0e230410ce06
Oliver Chaplin <ojc30@uclive.ac.nz>
2021-07-05 14:15:48 +1200

    S302T200-233 #time 1h 45m Implemented cucumber tests for task. Fixed an issue caused by cucumber tests attempting to delete items from repository. ?fix ?test

--- backend/src/main/java/org/seng302/project/serviceLayer/dto/card/CreateCardResponseDTO.java
+++ backend/src/main/java/org/seng302/project/serviceLayer/dto/card/CreateCardResponseDTO.java
@@ -2,12 +2,14 @@ package org.seng302.project.serviceLayer.dto.card;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * DTO object used as response for creating a card.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateCardResponseDTO {
 
     private Integer cardId;
