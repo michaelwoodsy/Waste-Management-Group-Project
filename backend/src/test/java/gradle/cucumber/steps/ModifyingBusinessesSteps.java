0fd5dda9c87509d7db831a3fce6ba4490b8306ed
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-07-22 10:52:15 +1200

    S302T200-273 #time 40m Implemented cucumber steps for both adding business images and making business images primary. ?test

--- backend/src/test/java/gradle/cucumber/steps/ModifyingBusinessesSteps.java
+++ backend/src/test/java/gradle/cucumber/steps/ModifyingBusinessesSteps.java
@@ -10,36 +10,48 @@ import org.junit.jupiter.api.BeforeEach;
 import org.seng302.project.AbstractInitializer;
 import org.seng302.project.repositoryLayer.model.Address;
 import org.seng302.project.repositoryLayer.model.Business;
+import org.seng302.project.repositoryLayer.model.Image;
 import org.seng302.project.repositoryLayer.model.User;
-import org.seng302.project.repositoryLayer.repository.AddressRepository;
-import org.seng302.project.repositoryLayer.repository.BusinessRepository;
-import org.seng302.project.repositoryLayer.repository.CardRepository;
-import org.seng302.project.repositoryLayer.repository.UserRepository;
+import org.seng302.project.repositoryLayer.repository.*;
 import org.seng302.project.serviceLayer.dto.address.AddressDTO;
+import org.seng302.project.serviceLayer.dto.business.AddBusinessImageDTO;
 import org.seng302.project.serviceLayer.dto.business.PostBusinessDTO;
+import org.seng302.project.serviceLayer.dto.user.AddUserImageDTO;
+import org.seng302.project.serviceLayer.service.BusinessImageService;
+import org.seng302.project.serviceLayer.util.SpringEnvironment;
 import org.seng302.project.webLayer.authentication.AppUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.transaction.Transactional;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 public class ModifyingBusinessesSteps extends AbstractInitializer {
 
     private final UserRepository userRepository;
     private final BusinessRepository businessRepository;
     private final AddressRepository addressRepository;
+    private final ImageRepository imageRepository;
     private final ObjectMapper objectMapper;
+
+    private final SpringEnvironment springEnvironment;
+
+    private final BusinessImageService businessImageService;
 
     private MockMvc mockMvc;
     private User testBusinessAdmin;
@@ -47,15 +59,23 @@ public class ModifyingBusinessesSteps extends AbstractInitializer {
     private PostBusinessDTO requestDTO;
     private RequestBuilder request;
 
+    private MockMultipartFile testImageFile;
+
     @Autowired
     public ModifyingBusinessesSteps(UserRepository userRepository,
                                     AddressRepository addressRepository,
                                     BusinessRepository businessRepository,
-                                    ObjectMapper objectMapper) {
+                                    ImageRepository imageRepository,
+                                    ObjectMapper objectMapper,
+                                    SpringEnvironment springEnvironment,
+                                    BusinessImageService businessImageService) {
         this.userRepository = userRepository;
         this.addressRepository = addressRepository;
         this.businessRepository = businessRepository;
+        this.imageRepository = imageRepository;
         this.objectMapper = objectMapper;
+        this.springEnvironment = springEnvironment;
+        this.businessImageService = businessImageService;
     }
 
     @BeforeEach
@@ -65,6 +85,7 @@ public class ModifyingBusinessesSteps extends AbstractInitializer {
         cardRepository.deleteAll();
         businessRepository.deleteAll();
         userRepository.deleteAll();
+        imageRepository.deleteAll();
 
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
@@ -83,7 +104,11 @@ public class ModifyingBusinessesSteps extends AbstractInitializer {
         testBusiness.setAdministrators(new ArrayList<>());
         addressRepository.save(testBusiness.getAddress());
         businessRepository.save(testBusiness);
+
+        testImageFile = this.getTestImageFile();
     }
+
+    //AC1
 
     @Given("I am the administrator of a business account")
     public void i_am_the_administrator_of_a_business_account() {
@@ -122,45 +147,114 @@ public class ModifyingBusinessesSteps extends AbstractInitializer {
         Assertions.assertEquals(requestDTO.getBusinessType(), testBusiness.getBusinessType());
     }
 
+    //AC2
+
     @Then("A Bad Request status is returned to the user")
     public void a_bad_request_status_is_returned_to_the_user() throws Exception {
         mockMvc.perform(request).andExpect(status().isBadRequest());
     }
 
+    //AC4
+
     @When("I upload a new image for the business")
     public void i_upload_a_new_image_for_the_business() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        AddBusinessImageDTO dto = new AddBusinessImageDTO(
+                testBusiness.getId(),
+                new AppUserDetails(testBusinessAdmin),
+                testImageFile
+        );
+        businessImageService.addBusinessImage(dto);
     }
 
     @Then("The image is saved to the repository for the business")
     public void the_image_is_saved_to_the_repository_for_the_business() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Optional<Business> optionalBusiness = businessRepository.findById(testBusiness.getId());
+        Assertions.assertTrue(optionalBusiness.isPresent());
+        Business retrievedBusiness = optionalBusiness.get();
+
+        Assertions.assertEquals(1, retrievedBusiness.getImages().size());
+        Image image = retrievedBusiness.getImages().get(0);
+
+        File imageFile = new File(springEnvironment.getMediaFolderPath() + image.getFilename());
+        File imageThumbnailFile = new File(springEnvironment.getMediaFolderPath() + image.getThumbnailFilename());
+        Assertions.assertTrue(imageFile.delete());
+        Assertions.assertTrue(imageThumbnailFile.delete());
     }
 
+    //AC5
+
     @Given("The business has at least {int} images, the first being the primary image")
-    public void the_business_has_at_least_images_the_first_being_the_primary_image(Integer numImages) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_business_has_at_least_images_the_first_being_the_primary_image(Integer numberOfImages) {
+        // numberOfImages = 2 so we add 2 images
+        AddBusinessImageDTO dto = new AddBusinessImageDTO(
+                testBusiness.getId(),
+                new AppUserDetails(testBusinessAdmin),
+                testImageFile
+        );
+        //Image 1
+        businessImageService.addBusinessImage(dto);
+        //Image 2
+        businessImageService.addBusinessImage(dto);
+
+        Optional<Business> optionalBusiness = businessRepository.findById(testBusiness.getId());
+        Assertions.assertTrue(optionalBusiness.isPresent());
+        Business retrivedBusiness = optionalBusiness.get();
+        Assertions.assertEquals(numberOfImages, retrivedBusiness.getImages().size());
+        testBusiness = retrivedBusiness;
     }
 
     @When("I change the primary image to be the second image")
     public void i_change_the_primary_image_to_be_the_second_image() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        businessImageService.setPrimaryImage(
+                testBusiness.getId(),
+                testBusiness.getImages().get(1).getId(),
+                new AppUserDetails(testBusinessAdmin));
     }
 
     @Then("The business' primary image is the second image")
     public void the_business_primary_image_is_the_second_image() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Optional<Business> optionalBusiness = businessRepository.findById(testBusiness.getId());
+        Assertions.assertTrue(optionalBusiness.isPresent());
+        Business retrievedBusiness = optionalBusiness.get();
+
+        Assertions.assertEquals(testBusiness.getImages().get(1), retrievedBusiness.getImages().get(1));
+
+        //Remove images and clean up
+        Image image1 = testBusiness.getImages().get(0);
+        File image1File = new File(springEnvironment.getMediaFolderPath() + image1.getFilename());
+        File image1ThumbnailFile = new File(springEnvironment.getMediaFolderPath() + image1.getThumbnailFilename());
+        Assertions.assertTrue(image1File.delete());
+        Assertions.assertTrue(image1ThumbnailFile.delete());
+
+        Image image2 = testBusiness.getImages().get(1);
+        File image2File = new File(springEnvironment.getMediaFolderPath() + image2.getFilename());
+        File image2ThumbnailFile = new File(springEnvironment.getMediaFolderPath() + image2.getThumbnailFilename());
+        Assertions.assertTrue(image2File.delete());
+        Assertions.assertTrue(image2ThumbnailFile.delete());
     }
+
+    //AC6
 
     @Then("A thumbnail of the new primary image is created")
     public void a_thumbnail_of_the_new_primary_image_is_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Optional<Business> optionalBusiness = businessRepository.findById(testBusiness.getId());
+        Assertions.assertTrue(optionalBusiness.isPresent());
+        Business retrievedBusiness = optionalBusiness.get();
+
+        Assertions.assertNotNull(retrievedBusiness.getImages().get(1).getThumbnailFilename());
+
+        //Remove images and clean up
+        Image image1 = testBusiness.getImages().get(0);
+        File image1File = new File(springEnvironment.getMediaFolderPath() + image1.getFilename());
+        File image1ThumbnailFile = new File(springEnvironment.getMediaFolderPath() + image1.getThumbnailFilename());
+        Assertions.assertTrue(image1File.delete());
+        Assertions.assertTrue(image1ThumbnailFile.delete());
+
+        Image image2 = testBusiness.getImages().get(1);
+        File image2File = new File(springEnvironment.getMediaFolderPath() + image2.getFilename());
+        File image2ThumbnailFile = new File(springEnvironment.getMediaFolderPath() + image2.getThumbnailFilename());
+        Assertions.assertTrue(image2File.delete());
+        Assertions.assertTrue(image2ThumbnailFile.delete());
     }
 
 }
--- backend/src/test/resources/gradle.cucumber/modifyingBusinesses.feature
+++ backend/src/test/resources/gradle.cucumber/modifyingBusinesses.feature
@@ -23,19 +23,19 @@ Feature: U11 - Modifying Businesses
 
 # TODO: Implement steps for AC3 once discussed
 
-#  Scenario: AC4: I can upload one or more images.
-#    Given I am the administrator of a business account
-#    When I upload a new image for the business
-#    Then The image is saved to the repository for the business
+  Scenario: AC4: I can upload one or more images.
+    Given I am the administrator of a business account
+    When I upload a new image for the business
+    Then The image is saved to the repository for the business
 
-#  Scenario: AC5: One of these images is deemed to be the primary image. I can change the primary image.
-#    Given I am the administrator of a business account
-#    And The business has at least 2 images, the first being the primary image
-#    When I change the primary image to be the second image
-#    Then The business' primary image is the second image
+  Scenario: AC5: One of these images is deemed to be the primary image. I can change the primary image.
+    Given I am the administrator of a business account
+    And The business has at least 2 images, the first being the primary image
+    When I change the primary image to be the second image
+    Then The business' primary image is the second image
 
-#  Scenario: AC6: A Thumbnail of the primary image is created
-#    Given I am the administrator of a business account
-#    And The business has at least 2 images, the first being the primary image
-#    When I change the primary image to be the second image
-#    Then A thumbnail of the new primary image is created
+  Scenario: AC6: A Thumbnail of the primary image is created
+    Given I am the administrator of a business account
+    And The business has at least 2 images, the first being the primary image
+    When I change the primary image to be the second image
+    Then A thumbnail of the new primary image is created
