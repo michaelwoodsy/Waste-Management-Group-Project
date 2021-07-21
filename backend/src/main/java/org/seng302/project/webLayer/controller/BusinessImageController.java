5f96841f3870435d308b95364663a2e816c2f57e
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-07-21 17:33:56 +1200

    S302T200-317 #time 15m Implemented setPrimaryImage controller and service, by looking at the same for user images. Still needs tests ?implement

--- backend/src/main/java/org/seng302/project/webLayer/controller/BusinessImageController.java
+++ backend/src/main/java/org/seng302/project/webLayer/controller/BusinessImageController.java
@@ -2,6 +2,10 @@ package org.seng302.project.webLayer.controller;
 
 import org.seng302.project.serviceLayer.dto.business.AddBusinessImageDTO;
 import org.seng302.project.serviceLayer.dto.business.AddBusinessImageResponseDTO;
+import org.seng302.project.serviceLayer.exceptions.ForbiddenException;
+import org.seng302.project.serviceLayer.exceptions.NoUserExistsException;
+import org.seng302.project.serviceLayer.exceptions.NotAcceptableException;
+import org.seng302.project.serviceLayer.exceptions.user.ForbiddenUserException;
 import org.seng302.project.serviceLayer.service.BusinessImageService;
 import org.seng302.project.webLayer.authentication.AppUserDetails;
 import org.slf4j.Logger;
@@ -34,5 +38,27 @@ public class BusinessImageController {
                                                 @RequestParam(value = "file") MultipartFile imageFile) {
         var requestDTO = new AddBusinessImageDTO(businessId, user, imageFile);
         return businessImageService.addBusinessImage(requestDTO);
+    }
+
+    /**
+     * Handles request to set a primary image for a business.
+     * @param businessId The Id of the business that you wish to change primary image of
+     * @param imageId The Id of the image you wish to make the primary image
+     * @param appUser The user that is trying to perform this action
+     */
+    @PutMapping("/businesses/{businessId}/images/{imageId}/makeprimary")
+    @ResponseStatus(HttpStatus.OK)
+    public void setPrimaryImage(@PathVariable Integer businessId, @PathVariable Integer imageId,
+                                @AuthenticationPrincipal AppUserDetails appUser) {
+
+        try {
+            businessImageService.setPrimaryImage(businessId, imageId, appUser);
+        } catch (NotAcceptableException | ForbiddenException handledException) {
+            logger.error(handledException.getMessage());
+            throw handledException;
+        } catch (Exception unhandledException) {
+            logger.error(String.format("Unexpected error while updating Businesses primary image: %s", unhandledException.getMessage()));
+        }
+
     }
 }
