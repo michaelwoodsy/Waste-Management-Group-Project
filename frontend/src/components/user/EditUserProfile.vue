007f0c83a187b5fc8af5461cf8f8314fac1fed7c
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-07-22 20:17:32 +1200

    S302T200-317 #time 20m Changed the way the users name and images are updated when editing user profile. ?implement

--- frontend/src/components/user/EditUserProfile.vue
+++ frontend/src/components/user/EditUserProfile.vue
@@ -716,11 +716,13 @@ export default {
         this.submitError = null
         this.submitting = false
         this.success = true
+        //Sets the correct user data (So the name changes in the nav bar)
+        let name = `${this.firstName} ${this.lastName}`
+        if (this.isEditingSelf && this.$root.$data.user.state.actingAs.name !== name) {
+          this.$root.$data.user.state.actingAs.name = name
+        }
+        this.$root.$data.user.updateData()
       })
-      //Sets the correct user data (So the name changes in the nav bar)
-      if (this.isEditingSelf) {
-        this.$root.$data.user.setLoggedIn(this.userId)
-      }
     },
 
     /**
