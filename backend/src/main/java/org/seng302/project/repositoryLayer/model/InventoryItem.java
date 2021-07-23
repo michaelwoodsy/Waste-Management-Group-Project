b49092bddbc89f7f034c75b633526ded210e9a2e
Oliver Chaplin <ojc30@uclive.ac.nz>
2021-07-23 14:00:44 +1200

    Revert "S302T200-312 #time 15m Made a small change to fix the schema of the database,..."
    This reverts commit 574725d49df180e25d8b3f90d541fe034dd5eeb4

--- backend/src/main/java/org/seng302/project/repositoryLayer/model/InventoryItem.java
+++ backend/src/main/java/org/seng302/project/repositoryLayer/model/InventoryItem.java
@@ -49,8 +49,10 @@ public class InventoryItem {
     }
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    @JoinColumn(name = "business_id", referencedColumnName = "business_id")
+    @JoinColumns({
+            @JoinColumn(name = "product_id"),
+            @JoinColumn(name = "business_id")
+    })
     public Product getProduct() {
         return this.product;
     }
--- backend/src/main/java/org/seng302/project/repositoryLayer/model/ProductId.java
+++ backend/src/main/java/org/seng302/project/repositoryLayer/model/ProductId.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 public class ProductId implements Serializable {
-
     private String id;
     private Integer businessId;
 
