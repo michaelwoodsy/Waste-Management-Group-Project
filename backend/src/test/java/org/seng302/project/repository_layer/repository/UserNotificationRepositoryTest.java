2503b1a507ea6c58ab0a84defcda67a8fc499475
Tom Rizzi <tcr59@uclive.ac.nz>
2021-08-05 16:13:55 +1200

    S302T200-360 #time 35m Implemented InterestedUserNotification and unit test. ?implement ?test
    - Had trouble with the DataJpaTest not working as I expected and the two tests ended up interfering with each other.

--- backend/src/test/java/org/seng302/project/repository_layer/repository/UserNotificationRepositoryTest.java
+++ backend/src/test/java/org/seng302/project/repository_layer/repository/UserNotificationRepositoryTest.java
@@ -1,9 +1,12 @@
 package org.seng302.project.repository_layer.repository;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.seng302.project.AbstractInitializer;
+import org.seng302.project.repository_layer.model.InterestedUserNotification;
 import org.seng302.project.repository_layer.model.PurchaserNotification;
+import org.seng302.project.repository_layer.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -15,18 +18,35 @@ class UserNotificationRepositoryTest extends AbstractInitializer {
 
     @Autowired
     private UserRepository userRepository;
-
-
+    
     @Autowired
     private AddressRepository addressRepository;
 
+    private User testUser;
+
+    @BeforeEach
+    void setup() {
+        testUser = userRepository.save(getTestUser());
+        testUser.setHomeAddress(addressRepository.save(getTestUser().getHomeAddress()));
+    }
+
+    /**
+     * Checks the PurchaseNotification entity can be saved to the UserNotification repository.
+     */
     @Test
     void purchaseNotification_whenSaved_noError() {
-        userRepository.save(getTestUser());
-        addressRepository.save(getTestUser().getHomeAddress());
-
         PurchaserNotification notification = new PurchaserNotification(
-                getTestUser(), getSaleListings().get(0), getTestUser().getHomeAddress());
+                testUser, getSaleListings().get(0), testUser.getHomeAddress());
+        Assertions.assertDoesNotThrow(() -> userNotificationRepository.save(notification));
+    }
+
+    /**
+     * Checks the PurchaseNotification entity can be saved to the UserNotification repository.
+     */
+    @Test
+    void interestedUserNotification_whenSaved_noError() {
+        InterestedUserNotification notification = new InterestedUserNotification(
+                getTestUser(), getSaleListings().get(0));
         Assertions.assertDoesNotThrow(() -> userNotificationRepository.save(notification));
     }
 
