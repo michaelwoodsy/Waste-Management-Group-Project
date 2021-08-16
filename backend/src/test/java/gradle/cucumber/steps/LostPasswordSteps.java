287339f7baa1cfdf926ee6b2b865c37c3a0bdf53
Sarah Bealing <smb272@uclive.ac.nz>
2021-08-16 12:58:56 +1200

    S302T200-381 #time 20m Wrote Cucumber scenarios and generated stubs for the lost password story.  ?test

---
+++ backend/src/test/java/gradle/cucumber/steps/LostPasswordSteps.java
@@ -0,0 +1,96 @@
+package gradle.cucumber.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+/**
+ * Cucumber steps for the U25 Lost password story
+ */
+public class LostPasswordSteps {
+
+    //AC3
+
+    @Given("I have requested to reset my password")
+    public void i_have_requested_to_reset_my_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I receive the password reset email message")
+    public void i_receive_the_password_reset_email_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The message includes a URL to the password reset screen")
+    public void the_message_includes_a_url_to_the_password_reset_screen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The message includes instructions for using the password reset URL")
+    public void the_message_includes_instructions_for_using_the_password_reset_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    //AC4
+
+    @Given("I have already used the password reset URL")
+    public void i_have_already_used_the_password_reset_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I try to use the password reset URL")
+    public void i_try_to_use_the_password_reset_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I am unable to reuse the URL")
+    public void i_am_unable_to_reuse_the_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("An hour has passed")
+    public void an_hour_has_passed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The URL has expired and I am unable to use it")
+    public void the_url_has_expired_and_i_am_unable_to_use_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    //AC5
+    @When("I try to reset my password to {string}")
+    public void i_try_to_reset_my_password_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Then I am informed it is not a valid password")
+    public void then_i_am_informed_it_is_not_a_valid_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("My old password is deleted")
+    public void my_old_password_is_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I can log in with my new password {string}")
+    public void i_can_log_in_with_my_new_password(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ backend/src/test/resources/gradle.cucumber/lostPassword.feature
@@ -0,0 +1,44 @@
+Feature: U25 Lost password
+
+  Scenario: AC3: An email message is sent to the user’s email address.
+  It contains a URL to password reset screen together with instructions (whose content is based on these ACs) for using it.
+    Given I have requested to reset my password
+    When I receive the password reset email message
+    Then The message includes a URL to the password reset screen
+
+
+  Scenario: AC3: An email message is sent to the user’s email address.
+  It contains a URL to password reset screen together with instructions (whose content is based on these ACs) for using it.
+    Given I have requested to reset my password
+    When I receive the password reset email message
+    Then The message includes instructions for using the password reset URL
+
+
+  Scenario: AC4:  The link can be used at most once (in case the user has remembered their password in the meantime)
+  and expires after a fixed period (e.g. an hour).
+    Given I have requested to reset my password
+    And I have already used the password reset URL
+    When I try to use the password reset URL
+    Then I am unable to reuse the URL
+
+  Scenario: AC4:  The link can be used at most once (in case the user has remembered their password in the meantime)
+  and expires after a fixed period (e.g. an hour).
+    Given I have requested to reset my password
+    And An hour has passed
+    When I try to use the password reset URL
+    Then The URL has expired and I am unable to use it
+
+
+  Scenario: AC5: A new password can be chosen.  It must obey the same rules as the original one.
+  The old password is deleted and the new one can then be used to log in.
+    Given I have requested to reset my password
+    When I try to reset my password to "notValid"
+    Then Then I am informed it is not a valid password
+
+
+  Scenario: AC5: A new password can be chosen.  It must obey the same rules as the original one.
+  The old password is deleted and the new one can then be used to log in.
+    Given I have requested to reset my password
+    When I try to reset my password to "ValidPa55word"
+    Then My old password is deleted
+    And I can log in with my new password "ValidPa55word"
