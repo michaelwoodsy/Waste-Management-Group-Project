6645b5f406ac914d6d6b794d265557175ef9b5f3
Niels van Antwerpen <nva40@uclive.ac.nz>
2021-09-04 15:57:53 +1200

    S302T200-430 #time 40m Finished off the tests for the service layer. Tried to implement the sale history into the abstract initializer but kept getting an error about things not being saved properly so just made them in the test class.  ?implement ?test

--- backend/src/test/java/org/seng302/project/service_layer/service/ResaleStatisticsServiceTest.java
+++ backend/src/test/java/org/seng302/project/service_layer/service/ResaleStatisticsServiceTest.java
@@ -12,13 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * Resale Statistics Service unit tests
  */
 @DataJpaTest
-public class ResaleStatisticsServiceTest extends AbstractInitializer {
+class ResaleStatisticsServiceTest extends AbstractInitializer {
 
     private final UserRepository userRepository;
     private final BusinessRepository businessRepository;
@@ -33,18 +32,6 @@ public class ResaleStatisticsServiceTest extends AbstractInitializer {
     private final UserNotificationRepository userNotificationRepository;
     private final SaleListingService saleListingService;
     private final ResaleStatisticsService resaleStatisticsService;
-
-    Business business1;
-    Integer business2Id;
-    User testUser;
-    User testOtherUser;
-    User testAdmin;
-    InventoryItem inventoryItem;
-    SaleListing saleListing1;
-    SaleListing saleListing2;
-    SaleListing saleListing3;
-    SaleListing saleListing4;
-    List<Sale> saleHistory;
 
     @Autowired
     public ResaleStatisticsServiceTest(UserRepository userRepository,
@@ -74,66 +61,67 @@ public class ResaleStatisticsServiceTest extends AbstractInitializer {
 
     @BeforeEach
     void setup() {
-        testUser = this.getTestUser();
+        User testUser = this.getTestUser();
         addressRepository.save(testUser.getHomeAddress());
         testUser.setId(null);
         testUser = userRepository.save(testUser);
 
-        this.testOtherUser = this.getTestOtherUser();
+        User testOtherUser = this.getTestOtherUser();
         addressRepository.save(testOtherUser.getHomeAddress());
         testOtherUser.setId(null);
         userRepository.save(testOtherUser);
 
-        testAdmin = this.getTestUserBusinessAdmin();
+        User testAdmin = this.getTestUserBusinessAdmin();
         addressRepository.save(testAdmin.getHomeAddress());
         testAdmin.setId(null);
         testAdmin = userRepository.save(testAdmin);
 
         Address address1 = new Address(null, null, "Rangiora", null, "Netherlands", null);
-        business1 = new Business("First Business", null, address1, "Retail Trade", testAdmin.getId());
+        Business business1 = new Business("First Business", null, address1, "Retail Trade", testAdmin.getId());
         addressRepository.save(address1);
         business1 = businessRepository.save(business1);
 
         Product product1 = new Product("TEST-1", "First Product", null, null, 5.00, business1.getId());
         productRepository.save(product1);
-        inventoryItem = new InventoryItem(product1, 10, null, null, "2021-01-01", null, null, "2021-12-01");
+        InventoryItem inventoryItem = new InventoryItem(product1, 10, null, null, "2021-01-01", null, null, "2021-12-01");
         inventoryItem = inventoryItemRepository.save(inventoryItem);
-        saleListing1 = new SaleListing(business1, inventoryItem, 10.00, null, LocalDateTime.parse("2021-08-25T00:00:00"), 5);
+        SaleListing saleListing1 = new SaleListing(business1, inventoryItem, 10.00, null, LocalDateTime.parse("2021-08-25T00:00:00"), 5);
         saleListing1 = saleListingRepository.save(saleListing1);
 
         Product product2 = new Product("TEST-2", "Second Product", null, null, 5.00, business1.getId());
         productRepository.save(product2);
         InventoryItem inventoryItem2 = new InventoryItem(product2, 10, null, null, "2021-01-01", null, null, "2021-12-02");
         inventoryItem2 = inventoryItemRepository.save(inventoryItem2);
-        saleListing2 = new SaleListing(business1, inventoryItem2, 15.00, null, LocalDateTime.parse("2021-10-25T00:00:00"), 10);
+        SaleListing saleListing2 = new SaleListing(business1, inventoryItem2, 15.00, null, LocalDateTime.parse("2021-10-25T00:00:00"), 10);
         saleListing2 = saleListingRepository.save(saleListing2);
 
         Address address2 = new Address(null, null, "Christchurch", null, "New Zealand", null);
         Business business2 = new Business("Second Business", null, address2, "Charitable Organisation", 1);
         addressRepository.save(address2);
         businessRepository.save(business2);
-        business2Id = business2.getId();
+        var business2Id = business2.getId();
 
         Product product3 = new Product("TEST-3", "Third Product", null, null, 5.00, business2.getId());
         productRepository.save(product3);
         InventoryItem inventoryItem3 = new InventoryItem(product3, 5, null, null, "2021-01-01", null, null, "2021-12-03");
         inventoryItem3 = inventoryItemRepository.save(inventoryItem3);
-        saleListing3 = new SaleListing(business2, inventoryItem3, 20.00, null, LocalDateTime.parse("2021-11-25T00:00:00"), 5);
+        SaleListing saleListing3 = new SaleListing(business2, inventoryItem3, 20.00, null, LocalDateTime.parse("2021-11-25T00:00:00"), 5);
         saleListingRepository.save(saleListing3);
+        Sale sale1 = new Sale(saleListing3);
+        saleHistoryRepository.save(sale1);
 
         Product product4 = new Product("TEST-4", "Fourth Product", null, null, 5.00, business2.getId());
         productRepository.save(product4);
         InventoryItem inventoryItem4 = new InventoryItem(product4, 5, null, null, "2021-01-01", null, null, "2021-12-04");
         inventoryItem4 = inventoryItemRepository.save(inventoryItem4);
-        saleListing4 = new SaleListing(business2, inventoryItem4, 30.00, null, LocalDateTime.parse("2021-12-25T00:00:00"), 5);
+        SaleListing saleListing4 = new SaleListing(business2, inventoryItem4, 30.00, null, LocalDateTime.parse("2021-12-25T00:00:00"), 5);
         saleListingRepository.save(saleListing4);
-
-        saleHistory = this.getSaleHistory();
-        saleHistoryRepository.saveAll(saleHistory);
+        Sale sale2 = new Sale(saleListing4);
+        saleHistoryRepository.save(sale2);
     }
 
     @Test
-    void getResaleStatistics_success_OK(){
+    void getResaleStatistics_success(){
         JSONObject response = resaleStatisticsService.getStatistics();
         Assertions.assertEquals(3, response.getAsNumber("totalUserCount"));
         Assertions.assertEquals(4, response.getAsNumber("numAvailableListings"));
