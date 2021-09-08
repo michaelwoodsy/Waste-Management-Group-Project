c3b40fa92f2285ae6ec5f66fcfb10f9281025c43
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-09-08 12:14:16 +1200

    S302T200-442 #time 20m Added pageable object to repository calls to limit results, also added a repository method to get popular without country so it gets popular worldwide. added docstrings to repository to show what they do. ?implement

--- backend/src/main/java/org/seng302/project/repository_layer/repository/LikedSaleListingRepository.java
+++ backend/src/main/java/org/seng302/project/repository_layer/repository/LikedSaleListingRepository.java
@@ -5,20 +5,57 @@ import org.seng302.project.repository_layer.model.SaleListing;
 import org.seng302.project.repository_layer.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
-public interface LikedSaleListingRepository extends JpaRepository<LikedSaleListing, Integer> {
+public interface LikedSaleListingRepository extends JpaRepository<LikedSaleListing, Integer>, PagingAndSortingRepository<LikedSaleListing, Integer> {
 
+    /**
+     * Finds all liked by sale listings and user
+     * @param listing listing to search by
+     * @param user user to search by
+     * @return List of LikedSaleListings
+     */
     List<LikedSaleListing> findByListingAndUser(SaleListing listing, User user);
 
+    /**
+     * Finds all liked sale listings by listing
+     * @param listing listing to search by
+     * @return List of LikedSaleListings
+     */
     List<LikedSaleListing> findAllByListing(SaleListing listing);
 
+    /**
+     * Finds all liked sale listings by user
+     * @param user user to search by
+     * @return List of LikedSaleListings
+     */
     List<LikedSaleListing> findAllByUser(User user);
 
+    /**
+     * Finds all liked sale listings by listing id
+     * @param listingId listing id to search by
+     * @return List of LikedSaleListings
+     */
     List<LikedSaleListing> findAllByListingId(@Param("listingId") Integer listingId);
 
+    /**
+     * finds the most popular sale listings by a specified country
+     * @param country country to get popular sale listings for
+     * @param pageable pageable object, to limit results
+     * @return A list made up of sale listing, count "pairs" (lists)
+     */
     @Query("select l.listing, count(l.listing) from LikedSaleListing l where l.listing.business.address.country = :country group by l.listing order by count(l.listing) desc")
-    List<List<Object>> findPopularByCountry(@Param("country") String country);
+    List<List<Object>> findPopularByCountry(@Param("country") String country, Pageable pageable);
+
+    /**
+     * finds the most popular sale listings for all countries
+     * @param pageable pageable object, to limit results
+     * @return A list made up of sale listing, count "pairs" (lists)
+     */
+    @Query("select l.listing, count(l.listing) from LikedSaleListing l group by l.listing order by count(l.listing) desc")
+    List<List<Object>> findPopular(Pageable pageable);
 
 }
