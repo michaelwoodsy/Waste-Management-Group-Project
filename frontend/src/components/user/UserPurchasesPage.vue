874b90c96a248378815528b2bb4a336e4888ee41
Rutger van Kruiningen <rva42@uclive.ac.nz>
2021-09-20 19:51:11 +1200

    S302T200-459 #time 30m Added sorting arrows to purchases fields, also fixed the displaying of data as the currency was being undefined ?implement ?fix

--- frontend/src/components/user/UserPurchasesPage.vue
+++ frontend/src/components/user/UserPurchasesPage.vue
@@ -17,11 +17,26 @@
       >
         <thead>
         <tr>
-          <th scope="col">Date Purchased</th>
-          <th scope="col">Product Name</th>
-          <th scope="col">Quantity</th>
-          <th scope="col">Price</th>
-          <th scope="col">Business</th>
+          <th class="pointer" scope="col" @click="orderSearch('datePurchased')">
+            <p class="d-inline">Date Purchased</p>
+            <p v-if="orderCol === 'datePurchased'" class="d-inline" style="margin-left: 5px">{{ orderDirArrow }}</p>
+          </th>
+          <th class="pointer" scope="col" @click="orderSearch('productName')">
+            <p class="d-inline">Product Name</p>
+            <p v-if="orderCol === 'productName'" class="d-inline" style="margin-left: 5px">{{ orderDirArrow }}</p>
+          </th>
+          <th class="pointer" scope="col" @click="orderSearch('quantity')">
+            <p class="d-inline">Quantity</p>
+            <p v-if="orderCol === 'quantity'" class="d-inline" style="margin-left: 5px">{{ orderDirArrow }}</p>
+          </th>
+          <th class="pointer" scope="col" @click="orderSearch('price')">
+            <p class="d-inline">Price</p>
+            <p v-if="orderCol === 'price'" class="d-inline" style="margin-left: 5px">{{ orderDirArrow }}</p>
+          </th>
+          <th class="pointer" scope="col" @click="orderSearch('business')">
+            <p class="d-inline">Business</p>
+            <p v-if="orderCol === 'business'" class="d-inline" style="margin-left: 5px">{{ orderDirArrow }}</p>
+          </th>
           <th scope="col">Review</th>
         </tr>
         </thead>
@@ -62,7 +77,57 @@ export default {
   name: "UserPurchasesPage",
   data() {
     return {
-      purchases: [
+      purchases: [],
+      error: null,
+      orderCol: null,
+      orderDirection: false, // False -> Ascending
+      resultsPerPage: 10,
+      totalCount: 0,
+      page: 1,
+      loading: false
+    }
+  },
+  components: {
+    PageWrapper,
+    LoginRequired
+  },
+  async mounted() {
+    this.purchases = await this.fillTable()
+  },
+  computed: {
+    /**
+     * Checks to see if user is logged in currently
+     * @returns {boolean|*}
+     */
+    isLoggedIn() {
+      return this.$root.$data.user.state.loggedIn
+    },
+
+    /**
+     * Gets the users' ID
+     * @returns {any}
+     */
+    userId() {
+      return this.$root.$data.user.state.userId
+    },
+
+    /**
+     * Checks which direction (ascending or descending) the order by should be
+     * @returns {string}
+     */
+    orderDirArrow() {
+      if (this.orderDirection) {
+        return '↓'
+      }
+      return '↑'
+    }
+  },
+  methods: {
+    /**
+     * Fills the table with the users purchases
+     */
+    async fillTable() {
+      let data = [
         {
           price: 15.00,
           dateSold: '2021-09-09',
@@ -94,41 +159,45 @@ export default {
           }
         },
       ]
-    }
-  },
-  components: {
-    PageWrapper,
-    LoginRequired
-  },
-  async mounted() {
-    await this.getCurrencies()
-  },
-  computed: {
-    /**
-     * Checks to see if user is logged in currently
-     * @returns {boolean|*}
-     */
-    isLoggedIn() {
-      return this.$root.$data.user.state.loggedIn
+      for (let purchase of data) {
+        purchase.currency = await this.$root.$data.product.getCurrency(purchase.currencyCountry)
+      }
+      return data
     },
 
     /**
-     * Gets the users' ID
-     * @returns {any}
+     * Function called when you click on one of the columns to order the results
+     * Makes another call to the backend to get the correct businesses when ordered
      */
-    userId() {
-      return this.$root.$data.user.state.userId
-    }
-  },
-  methods: {
-    /**
-     * Gets the currencies of the purchases so that their prices can be formatted.
-     */
-    async getCurrencies() {
-      for (let purchase of this.purchases) {
-        purchase.currency = await this.$root.$data.product.getCurrency(purchase.currencyCountry)
+    async orderSearch(sortBy) {
+      this.loading = true;
+      console.log(sortBy)
+
+      if (this.orderCol !== sortBy) {
+        this.orderDirection = false
+      } else {
+        this.orderDirection = !this.orderDirection
       }
+
+      this.orderCol = sortBy
+      if (!this.orderDirection) {
+        sortBy += "ASC"
+      } else {
+        sortBy += "DESC"
+      }
+
+      // try {
+      //   const res = await Business.getBusinesses(this.searchTerm, this.businessType, this.page - 1, sortBy)
+      //   this.error = null;
+      //   this.businesses = res.data[0];
+      //   this.totalCount = res.data[1];
+      //   this.loading = false;
+      // } catch (error) {
+      //   this.error = error;
+      //   this.loading = false;
+      // }
     },
+
     /**
      * Formats the date of the purchase
      * @return string date formatted like "DD/MM/YYYY hh:mm"
